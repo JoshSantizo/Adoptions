7d5a52c6c41286035c168d633486b524bfd4908a
Santizo <jsantizosanchez@gmail.com>
2024-03-17 21:27:30 -0600

    configuración de servicios, modulos y controladores

--- src/dogs/dogs.module.ts
+++ src/dogs/dogs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DogsController } from './dogs.controller';
 import { DogsService } from './dogs.service';
+import Dog from './entities/dog.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Dog])],
   controllers: [DogsController],
   providers: [DogsService]
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
+import User from './entities/user.entity';
+import CreateUserDto from './dtos/create-user.dto';
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    const records = this.usersService.findAll();
+    return records;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  @ApiCreatedResponse({
+    description: 'Este endpoint sirve para crear un usuario',
+    type: User,
+  })
+  create(@Body() body: CreateUserDto) {
+    return this.usersService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() body) {
+    return this.usersService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  destroy(@Param('id') id: number) {
+    return this.usersService.remove(id);
+  }
+}
